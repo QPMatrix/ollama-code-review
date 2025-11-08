7566f8d6f68ceca8cf358211c2422a476c479abc
QPMatrix <hasan@qpmatrix.tech>
2025-11-08 14:35:17 +0200

    Adding is mounted hook

---
+++ src/hooks/use-is-mounted/useIsMounted.ts
@@ -0,0 +1,15 @@
+import { useCallback, useEffect, useRef } from "react";
+
+export const useIsMounted = () => {
+	const isMountedRef = useRef(false);
+
+	useEffect(() => {
+		isMountedRef.current = true;
+		return () => {
+			isMountedRef.current = false;
+		};
+	}, []);
+
+	const isMounted = useCallback(() => isMountedRef.current, []);
+	return { isMounted };
+};
--- src/integrations/antd/AntdProvider.tsx
+++ src/integrations/antd/AntdProvider.tsx
@@ -1,28 +1,33 @@
 import { theme as antdTheme, ConfigProvider } from "antd";
 import type { FC, PropsWithChildren } from "react";
 import { useEffect, useState } from "react";
+import { useIsMounted } from "@/hooks/use-is-mounted/useIsMounted";
 import { darkColorTheme } from "./theme/dark";
 import { lightColorTheme } from "./theme/light";
 
 export const AntdProvider: FC<PropsWithChildren> = ({ children }) => {
 	const { defaultAlgorithm, darkAlgorithm } = antdTheme;
-
+	const { isMounted } = useIsMounted();
 	// Default to light mode for SSR and initial render
 	const [isDarkMode, setIsDarkMode] = useState(false);
 
 	useEffect(() => {
+		// This code runs only in browser, after mount
+		if (!isMounted()) return;
+
 		const mq = window.matchMedia("(prefers-color-scheme: dark)");
-		const handleChange = (e: MediaQueryListEvent) => setIsDarkMode(e.matches);
-
-		// set initial preference
-		setIsDarkMode(mq.matches);
-
-		// listen for changes
-		mq.addEventListener("change", handleChange);
-		return () => {
-			mq.removeEventListener("change", handleChange);
+		const applyTheme = () => {
+			if (mq.matches) {
+				setIsDarkMode(true);
+			} else {
+				setIsDarkMode(false);
+			}
 		};
-	}, []);
+
+		applyTheme();
+		mq.addEventListener("change", applyTheme);
+		return () => mq.removeEventListener("change", applyTheme);
+	}, [isMounted]);
 
 	return (
 		<ConfigProvider
